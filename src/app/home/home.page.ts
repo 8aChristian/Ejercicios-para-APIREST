0ef17fa46cd999d6bc1b2b959451de4b7c222dad
8aChristian <christian88afu@gmail.com>
2024-01-26 11:16:57 -0500

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PokeapiService } from '../Services/apiservice.service';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,53 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  pokemonId: number = 0;
+  pokemonName: string = "";
+  pokemonResultById: any;
+  pokemonResultByName: any;
+  itemId: number = 0;
+  itemName: string = "";
+  itemResultById: any;
+  itemResultByName: any;
+  constructor(private api: PokeapiService) {}
 
+  searchPokemonById() {
+    if (this.pokemonId) {
+      this.api.getPokemonID(this.pokemonId).subscribe(response => {
+        this.pokemonResultById = response;
+      }, error => {
+        console.log(error);
+      });
+    }
+  }
+
+  searchPokemonByName() {
+    if (this.pokemonName) {
+      this.api.getPokemonName(this.pokemonName).subscribe(response => {
+        this.pokemonResultByName = response;
+      }, error => {
+        console.log(error);
+      });
+    }
+  }
+
+  searchItemById() {
+    if (this.itemId) {
+      this.api.getItemID(this.itemId).subscribe(response => {
+        this.itemResultById = response;
+      }, error => {
+        console.log(error);
+      });
+    }
+  }
+
+  searchItemByName() {
+    if (this.itemName) {
+      this.api.getItemName(this.itemName).subscribe(response => {
+        this.itemResultByName = response;
+      }, error => {
+        console.log(error);
+      });
+    }
+  }
 }
